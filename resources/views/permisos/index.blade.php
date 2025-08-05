2a2ac9abb92598e487cd7a7d4e34701f08ad5f3a
Hollman <Hollmanpalma31@gmail.com>
2025-08-05 08:28:07 -0500

    feat: permisos index

--- resources/views/permisos/index.blade.php
+++ resources/views/permisos/index.blade.php
@@ -1,22 +1,15 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <x-principal>
+@push('styles')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/roles.css') }}">
     <link rel="stylesheet" href="{{ asset('css/principal.css') }}">
-    <title>Permisos</title>
-</head>
+@endpush
 
-<body>
-
-    <x-principal>
-        <div class="container py-4">
-            <h2 class="mb-4">Panel de Permisos</h2>
+@section('content')
+    <div class="container py-4">
+        <h2 class="mb-4">Panel de Permisos</h2>
 
             <!-- Dashboard -->
 
@@ -135,5 +128,5 @@
                 </div>
             </div>
         </div>
-
+@endsection 
     </x-principal>
